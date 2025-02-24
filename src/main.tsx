e763e5f5806862b351fcb3b1627f24425d54c7d5
Maksym <maksym.rudenko@eleks.com>
2025-02-24 15:01:32 +0200

    [React Training] Routing and pathing fix

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,10 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
-        <Route path="/artifact-tracker" element={<App />} />
+        <Route path="/" element={<App />} />
         <Route
           path="*"
-          element={<Navigate to="/artifact-tracker" replace />}
+          element={<Navigate to="/" replace />}
         />
       </Routes>
     </BrowserRouter>
